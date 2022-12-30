d335b9e9c1b9433916268764e9ad57a2535d4eff
Pim Zonneveld <pimzonneveld@pims-air.kpn>
2022-12-30 20:07:26 +0100

    Change port, expand gitignore

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3002;
 
 dotenv.config();
 
