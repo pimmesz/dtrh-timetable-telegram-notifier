4ea93a2d64ecf9f9329ff4be13b3825106f60f7c
Pim Zonneveld <pimzonneveld@pims-air.kpn>
2023-01-01 12:21:53 +0100

    Webhook GET debugger

--- routes/webhook.js
+++ routes/webhook.js
@@ -1,17 +1,18 @@
 import express from "express";
 import { respondToTelegramMessages } from "../utils/telegramUtils.js";
-import { getLineUp } from "../utils/spotifyUtils.js";
 const router = express.Router();
 import * as Telegram from "./../utils/telegramUtils.js";
 
 router.post("/", async (req, res) => {
-	console.log("message received", req.body.message.text);
-	await respondToTelegramMessages(req.body.message.text);
+	console.log("message received", req?.body?.message?.text);
 
-	if (req.body.message.text.toLowerCase() === "getlineup") {
-		await getLineUp();
+	if (req?.body?.message?.text) {
+		await respondToTelegramMessages(req.body.message.text);
+		res.send(req.body);
+		return;
 	}
-	res.send(req.body);
+
+	res.send("No message received");
 });
 
 router.get("/", async (req, res) => {
